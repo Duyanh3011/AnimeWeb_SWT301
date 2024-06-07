6b353cf1f7293a297bd62d0b503e66de4aee7eb5
DucLAHE182040 <duclahe182040@fpt.edu.vn>
2024-06-07 10:20:57 +0700

    Update StudioDao.java

--- src/java/dal/StudioDao.java
+++ src/java/dal/StudioDao.java
@@ -1,25 +1,24 @@
 package dal;
 
-import java.util.List;
-
-import model.Studio;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
+import model.Studio;
 
-public class StudioDao extends DBContext{
-	
-	public List<Studio> getAll() {
+public class StudioDao extends DBContext {
+
+    public List<Studio> getAll() {
         List<Studio> list = new ArrayList<>();
         String sql = "select * from [Studio]";
         //chay lenhj truy van
-        try {
-            PreparedStatement st = connection.prepareStatement(sql);
-            ResultSet rs = st.executeQuery();
-            while (rs.next()) {
-                Studio ad = new Studio(rs.getInt("id"), rs.getString("name"));
-                list.add(ad);
+        try (PreparedStatement st = connection.prepareStatement(sql);) {
+            try (ResultSet rs = st.executeQuery()) {
+                if (rs.next()) {
+                    Studio ad = new Studio(rs.getInt("id"), rs.getString("name"));
+                    list.add(ad);
+                }
             }
         } catch (SQLException e) {
             System.out.println(e);
@@ -27,9 +26,8 @@ public class StudioDao extends DBContext{
 
         return list;
     }
-	
-	
-        public Studio getStudioByID(int id) {
+
+    public Studio getStudioByID(int id) {
         String sql = "select * from Studio where id= ?";
         //chay lenhj truy van
         try {
@@ -48,25 +46,25 @@ public class StudioDao extends DBContext{
     }
 //	public void insert(Studio s){
 //        String sql = "INSERT INTO [User](id, password, email, fullname, admin)\n" + "VALUES (?, ?, ?, N'"+s.getFullname()+"', '0');";
-//        
+//
 //        try {
 //            PreparedStatement st = connection.prepareStatement(sql);
 //            st.setString(1, s.getId());
 //            st.setString(2, s.getPassword());
 //            st.setString(3, s.getEmail());
-//            
+//
 //            st.executeUpdate();
-//            
+//
 //        } catch (Exception e) {
 //        }
 //    }
 
-        public int mostStudio() {
+    public int mostStudio() {
         String sql = "SELECT top 1 s.id, s.name, COUNT(v.id) AS num_videos\n"
-                    + "	FROM Studio s\n"
-                    + "	LEFT JOIN Video v ON s.id = v.StudioId\n"
-                    + "	GROUP BY s.id, s.name\n"
-                    + "	ORDER BY num_videos DESC";
+                + "	FROM Studio s\n"
+                + "	LEFT JOIN Video v ON s.id = v.StudioId\n"
+                + "	GROUP BY s.id, s.name\n"
+                + "	ORDER BY num_videos DESC";
         //chay lenhj truy van
         try {
             PreparedStatement st = connection.prepareStatement(sql);
@@ -80,6 +78,7 @@ public class StudioDao extends DBContext{
 
         return -1;
     }
+
     public String mostStudioName() {
         String sql = "SELECT top 1 s.id, s.name, COUNT(v.id) AS num_videos\n"
                 + "	FROM Studio s\n"
@@ -99,13 +98,14 @@ public class StudioDao extends DBContext{
 
         return null;
     }
-        public static void main(String[] args) {
+
+    public static void main(String[] args) {
         StudioDao sd = new StudioDao();
-        List<Studio> li = sd. getAll();
+        List<Studio> li = sd.getAll();
         System.out.println(li.get(0).getName());
-        
+
         Studio ge = sd.getStudioByID(1);
-            System.out.println(ge.getName());
+        System.out.println(ge.getName());
 
     }
 }
